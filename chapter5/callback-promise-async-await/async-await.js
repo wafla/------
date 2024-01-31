99796464f8421d6c451da23cf1cc45c7c1fb4b83
최연우 <50083524+wafla@users.noreply.github.com>
2024-02-01 04:33:07 +0900

    first commit

---
+++ chapter5/callback-promise-async-await/async-await.js
@@ -0,0 +1,15 @@
+function waitOneSecond(msg){
+    return new Promise((resolve, _)=>{
+        setTimeout(()=>resolve(`${msg}`),1000);
+    });
+}
+
+async function countOneToTen(){
+    for (let x of [...Array(10).keys()]){
+        let result = await waitOneSecond(`${x+1}초 대기 중...`);
+        console.log(result);
+    }
+    console.log("완료");
+}
+
+countOneToTen();
